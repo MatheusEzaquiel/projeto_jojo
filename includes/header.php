b85e5ba822a73ff05b95ec9713a49b4eb8d58301
Matheus Bezerra <75776281+MatheusEzaquiel@users.noreply.github.com>
2022-04-13 03:22:21 -0300

    Initial commit

---
+++ includes/header.php
@@ -0,0 +1,14 @@
+<link rel="stylesheet" type="text/css" href="css/stylesheet-home.css">
+
+<header>
+	<img src="img/icon-nav-bar/gear60px.png" alt="settings" title="settings">
+	<nav>
+		<ul>
+			<li><a href="#">Joseph</a></li>
+			<li><a href="#">Stands</a></li>
+			<li><a href="about.php?acao=page-about">About</a></li>
+			<li><a href="home.php?acao=page-home">Home</a></li>
+		</ul>
+	</nav>
+
+</header>
---
+++ pages/home.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="shortcut icon" type="image/x-icon" href="img/favicon/favicon-j.png">
+	<title>Jojo Stands</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+	<h1 id="apresentacao">Jojo Stands</h1>
+	<p id=apresentacao>It's a blog about the stands of Jojo Bizarre Adventures</p>
+
+	<div class="container center">
+		<div class="item center"></div>
+		<div class="item center"></div>
+		<div class="item center"></div>
+	</div>
+
+</body>
+</html>
